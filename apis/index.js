0027458fb9e0229bb5233998263e74e943f1d81a
Ayoub-Marhr <marhraniayoub1@gmail.com>
2024-07-25 23:32:08 +0100

    connect to database

--- apis/index.js
+++ apis/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
+import mongoose from 'mongoose';
 const app = express();
-
+mongoose.connect('mongodb+srv://root:root@mern-blog.seiqug2.mongodb.net/?retryWrites=true&w=majority&appName=mern-blog').then(console.log('mongodb is connected')).catch(err=>{console.log(err)})
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
 });
