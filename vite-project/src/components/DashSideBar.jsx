49988d836e66dc5e38896504d2b525a138fb8a91
Ayoub-Marhr <marhraniayoub1@gmail.com>
2024-08-08 21:53:41 +0100

    add posts section to the dashboard

--- vite-project/src/components/DashSideBar.jsx
+++ vite-project/src/components/DashSideBar.jsx
@@ -1,15 +1,15 @@
-import { HiArrowSmRight, HiUser } from 'react-icons/hi';
+import { HiArrowSmRight, HiDocumentText, HiUser } from 'react-icons/hi';
 import { Sidebar } from 'flowbite-react';
 import { useLocation, Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { signoutSuccess } from '../redux/user/userSlice';
-
+import { useSelector } from 'react-redux';
 export default function DashSideBar() {
   const location = useLocation();
   const [tab, setTab] = useState('');
   const dispatch = useDispatch();
-
+  const {currentUser}=useSelector(state=>state.user)
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get('tab');
@@ -37,16 +37,29 @@ export default function DashSideBar() {
   return (
     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
-        <Sidebar.ItemGroup>
+        <Sidebar.ItemGroup className='flex flex-col gap-1'>
           <Link to='/dashboard?tab=profile'>
-            <Sidebar.Item 
-              active={tab === 'profile'}
-              icon={HiUser}
-              className="cursor-pointer"
-            >
-              Profile
-            </Sidebar.Item>
+          <Sidebar.Item 
+  active={tab === 'profile'}
+  label={currentUser.isAdmin ? 'Admin' : 'RocaUser'}
+  icon={HiUser}
+  className="cursor-pointer"
+>
+  Profile
+</Sidebar.Item>
+
           </Link>
+          {currentUser.isAdmin&&(
+            <Link to='/dashboard?tab=posts'>
+            <Sidebar.Item
+            active={tab==='posts'}
+            icon={HiDocumentText}
+            >
+              Posts
+            </Sidebar.Item>
+            </Link>
+          )}
+          
           <Sidebar.Item 
             href="#"
             icon={HiArrowSmRight}
